3544e17bbecc65b5f8d392bb308aa5f8fd9be544
awwa500@gmail.com <awwa500@gmail.com>
2016-07-06 22:36:15 +0900

    Add specs

--- spec/sg_spec.rb
+++ spec/sg_spec.rb
@@ -1,7 +1,48 @@
 require 'spec_helper'
 
-describe Sg do
-  it 'has a version number' do
-    expect(Sg::VERSION).not_to be nil
+module Sg
+  describe CLI do
+    it 'has a version number' do
+      expect(Sg::VERSION).not_to be nil
+    end
+
+    it 'GET https://api.sendgrid.com/v3/api_keys' do
+      args = %w(client api_keys get)
+      content = capture { CLI.start(args) }
+      expect(content).to match(/result/)
+    end
+
+    it 'GET https://api.sendgrid.com/v3/api_keys/YOUR_API_KEY_ID' do
+      args = %w(client api_keys example get)
+      content = capture { CLI.start(args) }
+      expect(content).to match(/errors/)
+    end
+
+    it 'GET https://api.sendgrid.com/v3/suppression/bounces?start_time=14324566&end_time=14324566' do
+      args = [
+        'client', 'suppression', 'bounces', 'get',
+        '--query-params', '{"start_time":14324566,"&end_time":14324566}'
+      ]
+      content = capture { CLI.start(args) }
+      expect(content).to match(/^\[.*\]$/)
+    end
+
+    it 'POST https://api.sendgrid.com/v3/api_keys' do
+      args = [
+        'client', 'api_keys', 'post',
+        '--request-body', '{"name": "My API Key", "scopes": ["mail.send"]}'
+      ]
+      content = capture { CLI.start(args) }
+      expect(content).to match(/api_key/)
+    end
+
+    it 'POST https://api.sendgrid.com/v3/mail/send' do
+      args = [
+        'client', 'mail', 'send', 'post',
+        '--request-body', '{"personalizations":[{"to":[{"email":"to@example.com"}],"subject":"Hello, World!"}],"from":{"email":"from@example.com"},"content":[{"type":"text","value":"Hello, World!"}]}'
+      ]
+      content = capture { CLI.start(args) }
+      expect(content).to match(//)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sg'
+
+def capture
+  begin
+    $stdout = StringIO.new
+    yield
+    result = $stdout.string
+  ensure
+    $stdout = STDOUT
+  end
+  result
+end
